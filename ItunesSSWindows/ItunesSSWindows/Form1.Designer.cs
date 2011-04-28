98d5577d60ad0e34f49696e5b82518cc9ef66efb
SND\startstop_cp <SND\startstop_cp@593edd63-b354-4607-8468-2ae7d70a7900>
2011-04-28 07:52:24 +0000

    Started work, but not functioning just yet.

---
+++ ItunesSSWindows/ItunesSSWindows/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace ItunesSSWindows
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txbStatus = new System.Windows.Forms.TextBox();
+            this.btnRunStats = new System.Windows.Forms.Button();
+            this.txbUserName = new System.Windows.Forms.TextBox();
+            this.txbPassword = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txbStatus
+            // 
+            this.txbStatus.Location = new System.Drawing.Point(25, 33);
+            this.txbStatus.Multiline = true;
+            this.txbStatus.Name = "txbStatus";
+            this.txbStatus.Size = new System.Drawing.Size(505, 121);
+            this.txbStatus.TabIndex = 0;
+            // 
+            // btnRunStats
+            // 
+            this.btnRunStats.Location = new System.Drawing.Point(454, 238);
+            this.btnRunStats.Name = "btnRunStats";
+            this.btnRunStats.Size = new System.Drawing.Size(75, 23);
+            this.btnRunStats.TabIndex = 1;
+            this.btnRunStats.Text = "Run";
+            this.btnRunStats.UseVisualStyleBackColor = true;
+            this.btnRunStats.Click += new System.EventHandler(this.btnRunStats_Click);
+            // 
+            // txbUserName
+            // 
+            this.txbUserName.Location = new System.Drawing.Point(142, 171);
+            this.txbUserName.Name = "txbUserName";
+            this.txbUserName.Size = new System.Drawing.Size(100, 20);
+            this.txbUserName.TabIndex = 2;
+            // 
+            // txbPassword
+            // 
+            this.txbPassword.Location = new System.Drawing.Point(142, 209);
+            this.txbPassword.Name = "txbPassword";
+            this.txbPassword.Size = new System.Drawing.Size(100, 20);
+            this.txbPassword.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(554, 285);
+            this.Controls.Add(this.txbPassword);
+            this.Controls.Add(this.txbUserName);
+            this.Controls.Add(this.btnRunStats);
+            this.Controls.Add(this.txbStatus);
+            this.Name = "Form1";
+            this.Text = "ItunesSS";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txbStatus;
+        private System.Windows.Forms.Button btnRunStats;
+        private System.Windows.Forms.TextBox txbUserName;
+        private System.Windows.Forms.TextBox txbPassword;
+    }
+}
+
---
+++ ItunesSSWindows/ItunesSSWindows/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ItunesSSWindows
+{
+    public partial class Form1 : Form
+    {
+
+        public statdropws.DeveloperAPI oDevAPI = new statdropws.DeveloperAPI();
+        const string APIKey = "1b50f643-fb89-4d5a-8fcf-20ca96deef22";
+        public string iTunesXML = ""; 
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public bool CheckAPIKey()
+        {
+            return oDevAPI.ValidateAPIKey(APIKey);                
+        }
+
+        public void AddLine(string Message)
+        {
+            txbStatus.Text += Message; 
+            txbStatus.Text += System.Environment.NewLine;
+        }
+
+        /// <summary>
+        /// Locate the ItunesXML
+        /// </summary>
+        public void LocateiTunesXML()
+        {
+            // Here we need to populate the string value with the location. 
+        }
+
+        /// <summary>
+        /// Parse the iTunes XML and then upload the stats to the user account
+        /// 
+        /// </summary>
+        public void ParseiTunesXML()
+        {
+            // Do some basic counts
+
+            // First one should be number of songs in the library. 
+
+            // We need a logged in user to add the stats to
+
+
+        }
+
+
+
+        private void btnRunStats_Click(object sender, EventArgs e)
+        {
+            AddLine(CheckAPIKey().ToString());
+            LocateiTunesXML();
+            ParseiTunesXML(); 
+        }
+    }
+}
